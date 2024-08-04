2611a8397978e542421b22a0337209fb8468c568
dyako <dyako.baram@gmail.com>
2024-08-04 15:23:43 +0300

    Add CustomerController and related files

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    public function index()
+    {
+        return Customer::all();
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'address' => 'required|string|max:255',
+            'phone' => 'required|string|max:15',
+            'balance' => 'required|numeric|min:0',
+        ]);
+
+        $customer = Customer::create($validated);
+
+        return response()->json($customer, 201);
+    }
+
+    public function show($id)
+    {
+        return Customer::findOrFail($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $customer = Customer::findOrFail($id);
+
+        $validated = $request->validate([
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'address' => 'required|string|max:255',
+            'phone' => 'required|string|max:15',
+            'balance' => 'required|numeric|min:0',
+        ]);
+
+        $customer->update($validated);
+
+        return response()->json($customer);
+    }
+
+    public function destroy($id)
+    {
+        $customer = Customer::findOrFail($id);
+        $customer->delete();
+
+        return response()->json(null, 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CustomerController;
 
-Route::middleware(['api'])->group(function () {
-    
+Route::middleware(['api','auth:api'])->group(function () {
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('change-password', [AuthController::class, 'changePassword']);
     Route::get('me', [AuthController::class, 'me']);
+
+    Route::get('customers', [CustomerController::class,'index']);
+
 });
 
 Route::middleware('api')->group(function (){
